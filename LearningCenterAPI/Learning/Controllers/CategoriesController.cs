cbdd428d39a4a3759eac82935fc815db78efb4fa
Mufment <jorleotuppiac@gmail.com>
2022-06-28 19:34:08 -0500

    feat categories: add controller

---
+++ LearningCenterAPI/Learning/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using LearningCenterAPI.Learning.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenterAPI.Learning.Controllers;
+
+
+[Route("/api/v1/[controller]")]
+public class CategoriesController :ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+
+    }
+    
+}
