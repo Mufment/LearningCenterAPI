2a848805b54a92229a7da1a993ad21479667aa70
Mufment <jorleotuppiac@gmail.com>
2022-06-28 19:40:41 -0500

    feat category: add model mapper

--- LearningCenterAPI/Learning/Domain/Models/Category.cs
+++ LearningCenterAPI/Learning/Domain/Models/Category.cs
@@ -3,7 +3,7 @@
 public class Category
 {
     public int Id { get; set; } 
-    public string name{ get; set; }
+    public string Name{ get; set; }
 
 
     public IList<Tutorial>Tutorials { get; set; }
---
+++ LearningCenterAPI/Learning/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LearningCenterAPI.Learning.Domain.Models;
+using LearningCenterAPI.Learning.Resources;
+
+namespace LearningCenterAPI.Learning.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Category, CategoryResource>();
+
+    }
+}
