595960570b42edf47de7a09e09e3fa15e5efba30
Mufment <jorleotuppiac@gmail.com>
2022-07-01 17:26:18 -0500

    feat Tutorials add tutorial
     add tutorial service and DBcontext relationships

---
+++ LearningCenterAPI/Learning/Domain/Repositories/ITutorialRepository.cs
@@ -0,0 +1,18 @@
+﻿using LearningCenterAPI.Learning.Domain.Models;
+
+namespace LearningCenterAPI.Learning.Domain.Repositories;
+
+public interface ITutorialRepository
+{
+    Task<IEnumerable<Tutorial>> ListAsync();
+
+    Task AddAsync(Tutorial tutorial);
+    Task<Tutorial> FindByIdAsync(int tutorialid);
+    Task<Tutorial> FindByNameAsync(string name);
+    Task<Tutorial> FindByCategoryIdAsync(int categoryId);
+    Task<IEnumerable<Tutorial>> DeleteAsync(int Tutorialid);
+
+    void Update(Tutorial tutorial);
+    void Remove(Tutorial tutorial);
+
+}
--- LearningCenterAPI/Learning/Persistance/Contexts/AppDbContext.cs
+++ LearningCenterAPI/Learning/Persistance/Contexts/AppDbContext.cs
@@ -20,6 +20,23 @@ public class AppDbContext : DbContext
         Builder.Entity<Category>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
         Builder.Entity<Category>().Property(p=>p.Name).IsRequired().HasMaxLength(30);
 
+        //relationships
+        Builder.Entity<Category>()
+            .HasMany(p=>p.Tutorials)
+            .WithOne(p=>p.Category)
+            .HasForeignKey(p=>p.CatergoryId)
+            
 
+        Builder.Entity<Tutorial>().ToTable("Tutorials");
+        Builder.Entity<Tutorial>().HasKey(p => p.Id);
+        Builder.Entity<Tutorial>().Property(p=>p.Id).IsRequired().ValueGeneratedOnAdd();
+        Builder.Entity<Tutorial>().Property(p=>p.Title).IsRequired().HasMaxLength(30);
+        Builder.Entity<Tutorial>().Property(p=>p.Description).IsRequired().HasMaxLength(300);
+        
+        
+        
     }
+    
+    
+    
 }
