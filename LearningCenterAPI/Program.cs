77349e5ab44a0a640df7c12a1a669c32503a0fc2
Mufment <jorleotuppiac@gmail.com>
2022-07-01 21:41:56 -0500

    add dependencies

--- LearningCenterAPI/Program.cs
+++ LearningCenterAPI/Program.cs
@@ -1,3 +1,11 @@
+using LearningCenterAPI.Learning.Domain.Repositories;
+using LearningCenterAPI.Learning.Domain.Services;
+using LearningCenterAPI.Learning.Mapping;
+using LearningCenterAPI.Learning.Persistance.Contexts;
+using LearningCenterAPI.Learning.Persistance.Repositories;
+using LearningCenterAPI.Learning.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +15,48 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+//Add LowerCase routes
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+
+//Dependency injection configuration
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, ICategoryService>();
+builder.Services.AddScoped<ITutorialRepository, TutorialRepository>();
+builder.Services.AddScoped<ITutorialService, TutorialService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+
+//AutoMapper Configuration
+
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile),
+    typeof(ResourceToModelProfile));
+
+
+
+////////////////////////////////////
+
 var app = builder.Build();
+
+//Validation for ensuring Database Objects are created
+
+using (var scope =app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
